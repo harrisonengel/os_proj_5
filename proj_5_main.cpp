048594f5ac0c724bde9cc40d40558a57d57c3a49
Harrison Engel <hengel1993@gmail.com>
2016-04-22 13:40:26 -0500

    Outline main.

--- proj_5_main.cpp
+++ proj_5_main.cpp
@@ -3,20 +3,38 @@
 #include <string.h>
 
 using namespace std;
+
+// Functions
+void parse_command(char *com);
+
 int main(int args, char* argv[])
 {
-  int numProc;
+  int numProc, rank, size;
   char command[256]
 
   cin >> numProc;
 
+  MPI_Init(&args, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+
   // Main Read loop
-  while(!"end".compare(command))
+  if(rank == 0)
+    {
+      printf ("[0]: There are %d processes in the system\n", numProc);
+      while(!"end".compare(command))
+	{
+	  cin >> command;
+	  
+	  parse_command(command);
+
+	}
+    }
+  else
     {
       
 
 
     }
-
 
 }
