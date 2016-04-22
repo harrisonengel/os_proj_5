a0218a0fde79df855da2edac08747df8b10ab1b6
Harrison Engel <hengel1993@gmail.com>
2016-04-21 21:05:12 -0500

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,4 @@
+all: proj_5_main
+
+proj_5_main: proj_5_main.cpp
+	mpic++ proj_5_main.cpp
---
+++ proj_5_main.cpp
@@ -0,0 +1,22 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+int main(int args, char* argv[])
+{
+  int numProc;
+  char command[256]
+
+  cin >> numProc;
+
+  // Main Read loop
+  while(!"end".compare(command))
+    {
+      
+
+
+    }
+
+
+}
